b92297528182e27707da8603e1ab5db488175464
Matt Shanahan <mrshanahan11235@gmail.com>
2017-05-14 07:09:17 -0500

    [2017] Made Store app more fancy - fun gifs & title!

--- 2017/Store/app/src/main/java/scav/sttrboy/store/MainActivity.java
+++ 2017/Store/app/src/main/java/scav/sttrboy/store/MainActivity.java
@@ -1,13 +1,49 @@
 package scav.sttrboy.store;
 
+import android.graphics.Canvas;
+import android.graphics.Movie;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCompletionListener {
+
+    private MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPlayer = MediaPlayer.create(MainActivity.this, R.raw.store);
+        mPlayer.setOnCompletionListener(this);
+        mPlayer.setVolume(1f, 1f);
+        mPlayer.seekTo(42000);
+    }
+
+    private void setVisibility(int id, boolean isVisible) {
+        ((TextView) findViewById(id)).setVisibility(isVisible ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPlayer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPlayer.pause();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        mp.start();
     }
 }
